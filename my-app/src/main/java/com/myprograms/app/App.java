c79cf9db18e94761314ba7dc1e2863832d308021
Crue Martin <cmartin@CAR.LOCAL>
2020-05-15 16:47:47 -0400

    Starting to get the JSON file reads worked out

--- my-app/src/main/java/com/myprograms/app/App.java
+++ my-app/src/main/java/com/myprograms/app/App.java
@@ -1,13 +1,57 @@
 package com.myprograms.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+//Json imports
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class App {
+
+    public static void main( String[] args ){
+    
+    JSONParser parse = new JSONParser();
+    
+    try{
+        Object obj = parse.parse(new FileReader("restaurants.json"));
+
+        JSONObject jo = (JSONObject) obj;
+
+        JSONArray restaurants = (JSONArray) jo.get("restaurant list");
+
+        Iterator itr = restaurants.iterator();
+
+        Iterator<Map.Entry> itr2; 
+
+        //int count = 0;
+        while(itr.hasNext()){
+
+            itr2 = ((Map) itr.next()).entrySet().iterator();
+            
+            while(itr2.hasNext()){
+                Map.Entry pair = itr2.next();
+                //System.out.println(pair.getKey() + " : " + pair.getValue());
+
+                JSONObject info = (JSONObject)pair.getValue();
+
+                System.out.println(". name: " + (String) info.get("name"));
+                 //count++;
+            }
+
+        }
+
+        
+
+
+    } catch(Exception e){
+        e.printStackTrace();
     }
+
+
+    }
+
 }
--- my-app/src/main/java/com/myprograms/app/Restaurant.java
+++ my-app/src/main/java/com/myprograms/app/Restaurant.java
@@ -1,3 +1,5 @@
+package com.myprograms.app;
+
 import java.util.Date;
 
 public class Restaurant{
